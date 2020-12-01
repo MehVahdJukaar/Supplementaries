97626a3959d438031ef8fcaee75eb904bd46534f
MehVahdJukaar <matteogozzi1998@gmail.com>
2020-12-01 19:29:01 +0100

    lanterns can now be placed correcly

--- src/main/java/net/mehvahdjukaar/supplementaries/events/RightClickEvent.java
+++ src/main/java/net/mehvahdjukaar/supplementaries/events/RightClickEvent.java
@@ -4,15 +4,18 @@ import net.mehvahdjukaar.supplementaries.blocks.WallLanternBlock;
 import net.mehvahdjukaar.supplementaries.blocks.WallLanternBlockTile;
 import net.mehvahdjukaar.supplementaries.configs.ServerConfigs;
 import net.mehvahdjukaar.supplementaries.setup.Registry;
+import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LanternBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.*;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
@@ -28,52 +31,59 @@ public class RightClickEvent {
     public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
         if(!ServerConfigs.cached.WALL_LANTERN_PLACEMENT)return;
         PlayerEntity player = event.getPlayer();
-
         if(!player.abilities.allowEdit)return;
 
         Hand hand = event.getHand();
-        if (hand != player.getActiveHand())return;
-
-        ItemStack stack = player.getHeldItem(hand);
+        ItemStack stack = event.getItemStack();
         Item i = stack.getItem();
-
-        if(i instanceof BlockItem && (((BlockItem) i).getBlock() instanceof LanternBlock || ((BlockItem) i).getBlock().getRegistryName().getNamespace().equals("skinnedlanterns"))){
+        //is lantern
+        if(i instanceof BlockItem && (((BlockItem) i).getBlock() instanceof LanternBlock || ((BlockItem) i).getBlock().getRegistryName().getNamespace().equals("skinnedlanterns"))) {
             Direction dir = event.getFace();
+            //is wall face
+            if (dir != Direction.UP && dir != Direction.DOWN) {
 
-            if(dir != Direction.UP && dir != Direction.DOWN){
+                World worldIn = event.getWorld();
                 BlockPos pos = event.getPos();
-                World world = event.getWorld();
+                BlockState blockstate = worldIn.getBlockState(pos);
+                //try interact with block behind
+                BlockRayTraceResult raytrace = new BlockRayTraceResult(
+                        new Vector3d(pos.getX(), pos.getY(), pos.getZ()), dir, pos, false);
 
+               if(!player.isSneaking()) {
+                    ActionResultType interactresult = blockstate.onBlockActivated(worldIn, player, hand, raytrace);
+                    //interacted with block
+                    if (interactresult.isSuccessOrConsume()) {
+                        if (player instanceof ServerPlayerEntity)
+                            CriteriaTriggers.RIGHT_CLICK_BLOCK_WITH_ITEM.test((ServerPlayerEntity) player, pos, stack);
+                        event.setCanceled(true);
+                        event.setCancellationResult(interactresult);
+                        return;
+                    }
+                }
+
+                //place lantern
                 Item item = Registry.WALL_LANTERN_ITEM;
 
                 BlockItemUseContext ctx = new BlockItemUseContext(
-                        new ItemUseContext(player, hand, new BlockRayTraceResult(
-                                new Vector3d(pos.getX(),pos.getY(),pos.getZ()), dir, pos, false)));
+                        new ItemUseContext(player, hand, raytrace));
+                ActionResultType placeresult = ((BlockItem)item).tryPlace(ctx);
 
-                ActionResultType result = ((BlockItem)item).tryPlace(ctx);
+                if(placeresult.isSuccessOrConsume()){
+                    //swing animation
+                    if(player instanceof ServerPlayerEntity)
+                        CriteriaTriggers.RIGHT_CLICK_BLOCK_WITH_ITEM.test((ServerPlayerEntity) player, pos, stack);
+                    event.setCanceled(true);
+                    event.setCancellationResult(placeresult);
 
-                if(result.isSuccessOrConsume()) {
-
+                    //update tile
                     BlockState s = ((BlockItem) i).getBlock().getDefaultState();
-                    BlockPos placedPos = ctx.getPos();
-                    //update light level
-                    world.setBlockState(placedPos, world.getBlockState(placedPos)
-                            .with(WallLanternBlock.LIGHT_LEVEL, s.getLightValue()),4|16);
-
-
-
-                    TileEntity te = world.getTileEntity(placedPos);
+                    BlockPos placedpos = ctx.getPos();
+                    TileEntity te = worldIn.getTileEntity(placedpos);
                     if (te instanceof WallLanternBlockTile) {
                         ((WallLanternBlockTile) te).lanternBlock = s;
+                        worldIn.setBlockState(placedpos, worldIn.getBlockState(placedpos)
+                                .with(WallLanternBlock.LIGHT_LEVEL, s.getLightValue()),4|16);
                     }
-
-                    player.swing(hand, true);
-
-                    SoundType soundtype = s.getSoundType(world, ctx.getPos(), player);
-                    //world.playSound(null, ctx.getPos(), soundtype.getPlaceSound(), SoundCategory.BLOCKS, (soundtype.getVolume() + 1.0F) / 2.0F, soundtype.getPitch() * 0.8F);
-
-                    event.setCanceled(true);
-
                 }
             }
         }
