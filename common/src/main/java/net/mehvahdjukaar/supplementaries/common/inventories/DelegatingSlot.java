67b4a2b20d1371868db94686f890c467a335118d
MehVahdJukaar <matteogozzi1998@gmail.com>
2023-06-13 19:35:53 +0200

    slot client desync "fix"

--- common/src/main/java/net/mehvahdjukaar/supplementaries/common/inventories/DelegatingSlot.java
+++ common/src/main/java/net/mehvahdjukaar/supplementaries/common/inventories/DelegatingSlot.java
@@ -1,15 +1,17 @@
 package net.mehvahdjukaar.supplementaries.common.inventories;
 
+import net.mehvahdjukaar.supplementaries.common.block.tiles.SafeBlockTile;
 import net.mehvahdjukaar.supplementaries.common.utils.MiscUtils;
 import net.minecraft.world.Container;
+import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BaseContainerBlockEntity;
 
 public class DelegatingSlot extends Slot {
-    public DelegatingSlot(Container inventory, int index, int xPosition, int yPosition) {
+    public DelegatingSlot(BaseContainerBlockEntity inventory, int index, int xPosition, int yPosition) {
         super(inventory, index, xPosition, yPosition);
     }
-
     @Override
     public boolean mayPlace(ItemStack stack) {
         return this.container.canPlaceItem(this.index, stack);
