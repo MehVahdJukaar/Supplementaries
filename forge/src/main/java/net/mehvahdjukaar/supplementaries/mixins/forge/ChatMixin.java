8896a64359c76e042f367e92b4bd14cdaf12ef41
MehVahdJukaar <matteogozzi1998@gmail.com>
2023-08-22 21:41:29 +0200

    a

--- forge/src/main/java/net/mehvahdjukaar/supplementaries/mixins/forge/ChatMixin.java
+++ forge/src/main/java/net/mehvahdjukaar/supplementaries/mixins/forge/ChatMixin.java
@@ -4,6 +4,7 @@ import einstein.jmc.JustMoreCakes;
 import einstein.jmc.JustMoreCakesForge;
 import net.mehvahdjukaar.supplementaries.client.renderers.forge.PicklePlayer;
 import net.minecraft.client.gui.screens.ChatScreen;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.block.Block;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ChatScreen.class)
 public abstract class ChatMixin {
 
-    @Inject(method = "handleChatInput", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "handleChatInput", at = @At("HEAD"))
     private void interceptMessage(String string, boolean bl, CallbackInfoReturnable<Boolean> callback) {
         PicklePlayer.onChatEvent(string);
     }
