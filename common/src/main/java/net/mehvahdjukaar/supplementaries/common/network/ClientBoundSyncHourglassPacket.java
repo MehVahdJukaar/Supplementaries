2046dc55feac3ac04f49e059b24bf624d0738b5e
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-06-22 20:35:21 +0200

    server stuff fix 2

--- common/src/main/java/net/mehvahdjukaar/supplementaries/common/network/ClientBoundSyncHourglassPacket.java
+++ common/src/main/java/net/mehvahdjukaar/supplementaries/common/network/ClientBoundSyncHourglassPacket.java
@@ -3,7 +3,6 @@ package net.mehvahdjukaar.supplementaries.common.network;
 import com.mojang.serialization.DataResult;
 import net.mehvahdjukaar.moonlight.api.platform.network.ChannelHandler;
 import net.mehvahdjukaar.moonlight.api.platform.network.Message;
-import net.mehvahdjukaar.moonlight.api.util.Utils;
 import net.mehvahdjukaar.supplementaries.Supplementaries;
 import net.mehvahdjukaar.supplementaries.common.block.hourglass.HourglassTimeData;
 import net.mehvahdjukaar.supplementaries.common.block.hourglass.HourglassTimesManager;
@@ -11,7 +10,6 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.resources.RegistryOps;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -31,8 +29,7 @@ public class ClientBoundSyncHourglassPacket implements Message {
         for (int i = 0; i < size; i++) {
             CompoundTag tag = buf.readNbt();
             if (tag != null) {
-                DataResult<HourglassTimeData> r = HourglassTimeData.NETWORK_CODEC.parse(RegistryOps.create(
-                        NbtOps.INSTANCE, Utils.hackyGetRegistryAccess()), tag);
+                DataResult<HourglassTimeData> r = HourglassTimeData.NETWORK_CODEC.parse(NbtOps.INSTANCE, tag);
                 r.result().ifPresent(hourglass::add);
             }
         }
@@ -42,8 +39,7 @@ public class ClientBoundSyncHourglassPacket implements Message {
     public void writeToBuffer(FriendlyByteBuf buf) {
         buf.writeInt(this.hourglass.size());
         for (var entry : this.hourglass) {
-            DataResult<Tag> r = HourglassTimeData.NETWORK_CODEC.encodeStart(RegistryOps.create(
-                    NbtOps.INSTANCE, Utils.hackyGetRegistryAccess()), entry);
+            DataResult<Tag> r = HourglassTimeData.NETWORK_CODEC.encodeStart(NbtOps.INSTANCE, entry);
             if (r.result().isPresent()) {
                 buf.writeNbt((CompoundTag) r.result().get());
             }
