ac59b187984392c3c8407ace561eff0cb61cd5ac
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-11-02 08:59:27 +0100

    Revert "stuffto rollback"
    This reverts commit 0b9bba9ff18f9e0ce03c286cc86b91a7c2fe553b.

--- common/src/main/java/net/mehvahdjukaar/supplementaries/reg/ModRegistry.java
+++ common/src/main/java/net/mehvahdjukaar/supplementaries/reg/ModRegistry.java
@@ -35,7 +35,6 @@ import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.properties.BlockSetType;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraft.world.level.material.PushReaction;
@@ -698,9 +697,8 @@ public class ModRegistry {
     ));
 
     //netherite doors
-    public static final Supplier<Block> NETHERITE_DOOR = regBlock(NETHERITE_DOOR_NAME, () ->new DoorBlock(BlockSetType.IRON,
+    public static final Supplier<Block> NETHERITE_DOOR = regBlock(NETHERITE_DOOR_NAME, () -> new NetheriteDoorBlock(
             BlockBehaviour.Properties.ofFullCopy(Blocks.NETHERITE_BLOCK)
-
                     .noOcclusion()
     ));
     public static final Supplier<Item> NETHERITE_DOOR_ITEM = regItem(NETHERITE_DOOR_NAME, () -> new BlockItem(
