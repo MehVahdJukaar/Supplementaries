b751698230966f7b26798cf3edda644a77dc7008
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-11-04 23:21:17 +0100

    cabin siding 3

--- changelog.md
+++ changelog.md
@@ -1,5 +1,5 @@
 CHECK 3.0.0 PATCH NOTES!!
 
-- fixed an issue with mods adding other post shaders
-- updated flywheel soft dep & it not initializing on fabric
-- fixed wall sign post placement on replaceable blocks
+- added cabin siding block
+- improved shader apply code
+- fixed an issue with sign post placement
--- common/src/main/java/net/mehvahdjukaar/supplementaries/configs/CommonConfigs.java
+++ common/src/main/java/net/mehvahdjukaar/supplementaries/configs/CommonConfigs.java
@@ -488,6 +488,7 @@ public class CommonConfigs {
             STATUE_ENABLED = feature(builder, ModConstants.STATUE_NAME);
             DOORMAT_ENABLED = feature(builder, ModConstants.DOORMAT_NAME);
             FLINT_BLOCK_ENABLED = feature(builder, ModConstants.FLINT_BLOCK_NAME);
+            CABIN_SIDING_ENABLED = feature(builder, ModConstants.CABIN_SIDING_NAME);
             CANDLE_HOLDER_ENABLED = feature(builder, ModConstants.CANDLE_HOLDER_NAME);
             FIRE_PIT_ENABLED = feature(builder, ModConstants.FIRE_PIT_NAME);
             WICKER_FENCE_ENABLED = feature(builder, ModConstants.WICKER_FENCE_NAME);
@@ -586,6 +587,7 @@ public class CommonConfigs {
         public static final Supplier<Boolean> FEATHER_BLOCK_ENABLED;
 
         public static final Supplier<Boolean> FLINT_BLOCK_ENABLED;
+        public static final Supplier<Boolean> CABIN_SIDING_ENABLED;
 
         public static final Supplier<Boolean> DOORMAT_ENABLED;
 
