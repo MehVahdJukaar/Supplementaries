588502ca8e56bc05a56ce653918ad257d2b5d074
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-11-10 03:34:07 +0100

    data stuff

--- neoforge/src/main/java/net/mehvahdjukaar/supplementaries/reg/neoforge/ModCommandsImpl.java
+++ neoforge/src/main/java/net/mehvahdjukaar/supplementaries/reg/neoforge/ModCommandsImpl.java
@@ -1,28 +1,10 @@
 package net.mehvahdjukaar.supplementaries.reg.neoforge;
 
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.HitResult;
-import net.neoforged.neoforge.common.Tags;
-import net.neoforged.neoforge.common.extensions.IBlockStateExtension;
-
 public class ModCommandsImpl {
 
     public static void registerArguments() {
-
     }
 
-    public static class a extends Block {
-
-        public a(Properties properties) {
-            super(properties);
-        }
-    }
 
 }
