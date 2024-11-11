ad121c8ba276f2e543cbbcc32ac17e16b509efe1
Niklas Widmann <nik@somethingcatchy.net>
2024-11-11 09:55:17 +0100

    add IIngredientSubtypeInterpreter for buntings
    (cherry picked from commit 229f60342c9feb37f35b04cb48b24e730c3cc16d)

--- common/src/main/java/net/mehvahdjukaar/supplementaries/integration/JEICompat.java
+++ common/src/main/java/net/mehvahdjukaar/supplementaries/integration/JEICompat.java
@@ -6,11 +6,15 @@ import mezz.jei.api.constants.RecipeTypes;
 import mezz.jei.api.registration.IRecipeRegistration;
 import net.mehvahdjukaar.supplementaries.Supplementaries;
 import net.mehvahdjukaar.supplementaries.common.items.crafting.SpecialRecipeDisplays;
+import net.mehvahdjukaar.supplementaries.reg.ModRegistry;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.crafting.CraftingRecipe;
 import net.minecraft.world.item.crafting.RecipeHolder;
 
 import java.util.List;
+import java.util.Optional;
 
 @JeiPlugin
 public class JEICompat implements IModPlugin {
@@ -27,6 +31,18 @@ public class JEICompat implements IModPlugin {
         if (!CompatHandler.REI && !CompatHandler.EMI) {
             SpecialRecipeDisplays.registerCraftingRecipes(r -> registry.addRecipes(RecipeTypes.CRAFTING,
                     (List<RecipeHolder<CraftingRecipe>>) (List) r));
+        }
+    }
+
+    public  enum BuntingSubtypeInterpreter implements IIngredientSubtypeInterpreter<ItemStack> {
+        INSTANCE;
+
+        public String apply(ItemStack itemStack, UidContext uidContext) {
+
+            Optional<String> color = Optional.ofNullable(itemStack.getTag())
+                    .map(tag -> tag.getString("Color"));
+
+            return color.orElse("");
         }
     }
 
