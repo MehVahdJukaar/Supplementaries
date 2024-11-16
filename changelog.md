c3e8b03e6dd4aec49ac007394988ceb9d50f92e2
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-11-16 03:29:15 +0100

    lumisene particle

--- changelog.md
+++ changelog.md
@@ -1,6 +1,4 @@
 CHECK 3.0.0 PATCH NOTES!!
 
-- changed awning models
-- fixed some missing lang lines
-- fixed lumisene bottle not giving flammable
-- added jeed files for lumisene bucket
+- improved some things about lumisene
+- cannon trajectory display is now server driven
--- common/src/main/java/net/mehvahdjukaar/supplementaries/common/fluids/LumiseneFluid.java
+++ common/src/main/java/net/mehvahdjukaar/supplementaries/common/fluids/LumiseneFluid.java
@@ -3,7 +3,6 @@ package net.mehvahdjukaar.supplementaries.common.fluids;
 import net.mehvahdjukaar.supplementaries.reg.ModFluids;
 import net.mehvahdjukaar.supplementaries.reg.ModParticles;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -22,16 +21,14 @@ public class LumiseneFluid extends FiniteFluid {
         if (random.nextInt(12) == 0) {
             BlockState above = level.getBlockState(pos.above());
             if (!above.getFluidState().isEmpty()) return;
-            if (above.isAir() || !above.isFaceSturdy(level, pos.above(), Direction.DOWN)) {
-                BlockState blockState = level.getBlockState(pos);
-                if (blockState.getBlock() instanceof FlammableLiquidBlock fb &&
-                        !fb.isLitUp(blockState, level, pos)) {
-                    level.addParticle(ModParticles.SPARKLE_PARTICLE.get(),
-                            pos.getX() + random.nextDouble(),
-                            pos.getY() + getOwnHeight(state) + 1 / 32f,
-                            pos.getZ() + random.nextDouble(),
-                            0, 0, 0);
-                }
+            BlockState blockState = level.getBlockState(pos);
+            if (blockState.getBlock() instanceof FlammableLiquidBlock fb &&
+                    !fb.isLitUp(blockState, level, pos)) {
+                level.addParticle(ModParticles.SPARKLE_PARTICLE.get(),
+                        pos.getX() + random.nextDouble(),
+                        pos.getY() + getOwnHeight(state) + 1 / 32f,
+                        pos.getZ() + random.nextDouble(),
+                        0, 0, 0);
             }
         }
     }
