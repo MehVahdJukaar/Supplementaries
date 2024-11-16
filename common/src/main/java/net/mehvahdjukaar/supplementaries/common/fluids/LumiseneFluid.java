5eb016c8b2771d383ae96fd54fb7c42d8b82dbb4
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-11-16 01:59:52 +0100

    lumisene particle and overlay

--- common/src/main/java/net/mehvahdjukaar/supplementaries/common/fluids/LumiseneFluid.java
+++ common/src/main/java/net/mehvahdjukaar/supplementaries/common/fluids/LumiseneFluid.java
@@ -3,6 +3,7 @@ package net.mehvahdjukaar.supplementaries.common.fluids;
 import net.mehvahdjukaar.supplementaries.reg.ModFluids;
 import net.mehvahdjukaar.supplementaries.reg.ModParticles;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -19,14 +20,18 @@ public class LumiseneFluid extends FiniteFluid {
     protected void animateTick(Level level, BlockPos pos, FluidState state, RandomSource random) {
         super.animateTick(level, pos, state, random);
         if (random.nextInt(12) == 0) {
-            BlockState blockState = level.getBlockState(pos);
-            if (blockState.getBlock() instanceof FlammableLiquidBlock fb &&
-                    !fb.isLitUp(blockState, level, pos)) {
-                level.addParticle(ModParticles.SPARKLE_PARTICLE.get(),
-                        pos.getX() + random.nextDouble(),
-                        pos.getY() + getOwnHeight(state) + 1 / 32f,
-                        pos.getZ() + random.nextDouble(),
-                        0, 0, 0);
+            BlockState above = level.getBlockState(pos.above());
+            if (!above.getFluidState().isEmpty()) return;
+            if (above.isAir() || !above.isFaceSturdy(level, pos.above(), Direction.DOWN)) {
+                BlockState blockState = level.getBlockState(pos);
+                if (blockState.getBlock() instanceof FlammableLiquidBlock fb &&
+                        !fb.isLitUp(blockState, level, pos)) {
+                    level.addParticle(ModParticles.SPARKLE_PARTICLE.get(),
+                            pos.getX() + random.nextDouble(),
+                            pos.getY() + getOwnHeight(state) + 1 / 32f,
+                            pos.getZ() + random.nextDouble(),
+                            0, 0, 0);
+                }
             }
         }
     }
