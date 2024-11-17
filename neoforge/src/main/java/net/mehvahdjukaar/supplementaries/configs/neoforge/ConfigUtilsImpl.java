7058bf6f048d1ba81f24235be7a7cea97ce4465d
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-11-18 00:59:12 +0100

    tag fixes

--- neoforge/src/main/java/net/mehvahdjukaar/supplementaries/configs/neoforge/ConfigUtilsImpl.java
+++ neoforge/src/main/java/net/mehvahdjukaar/supplementaries/configs/neoforge/ConfigUtilsImpl.java
@@ -1,5 +1,6 @@
 package net.mehvahdjukaar.supplementaries.configs.neoforge;
 
+import net.mehvahdjukaar.moonlight.api.resources.recipe.neoforge.ResourceConditionsBridge;
 import net.mehvahdjukaar.supplementaries.integration.CompatHandler;
 import net.mehvahdjukaar.supplementaries.integration.neoforge.configured.ModConfigSelectScreen;
 import net.minecraft.client.Minecraft;
