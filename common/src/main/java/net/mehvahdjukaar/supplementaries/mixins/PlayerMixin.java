8acc7463941a671531b13200631ced4caa3e10c6
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-11-17 01:24:41 +0100

    player mixin missing from 1.21 for some reason

---
+++ common/src/main/java/net/mehvahdjukaar/supplementaries/mixins/PlayerMixin.java
@@ -0,0 +1,18 @@
+package net.mehvahdjukaar.supplementaries.mixins;
+
+import net.mehvahdjukaar.supplementaries.api.IQuiverEntity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Player.class)
+public class PlayerMixin implements IQuiverEntity {
+    @Override
+    public ItemStack supplementaries$getQuiver() {
+        return ItemStack.EMPTY;
+    }
+
+    @Override
+    public void supplementaries$setQuiver(ItemStack quiver) {
+    }
+}
