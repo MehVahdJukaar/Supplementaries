9b92956f3390db737c4c6b4e98449acac5f6aa55
MehVahdJukaar <matteogozzi1998@gmail.com>
2024-12-02 19:01:49 +0100

    a

--- common/src/main/java/net/mehvahdjukaar/supplementaries/ClientProxy.java
+++ common/src/main/java/net/mehvahdjukaar/supplementaries/ClientProxy.java
@@ -1,13 +1,11 @@
 package net.mehvahdjukaar.supplementaries;
 
-import net.mehvahdjukaar.moonlight.api.platform.PlatHelper;
-import net.mehvahdjukaar.moonlight.api.platform.RegHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.player.Player;
 
 public class ClientProxy {
 
-   public static Player getLocalPlayer() {
+    public static Player getLocalPlayer() {
         return Minecraft.getInstance().player;
     }
 }
