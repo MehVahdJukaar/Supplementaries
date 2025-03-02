5c0a7718270b306b06126225d50cf7f52735098f
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-03-02 09:06:25 +0100

    changelog

---
+++ fabric/src/main/java/net/mehvahdjukaar/supplementaries/integration/fabric/CreateCompatImpl.java
@@ -0,0 +1,14 @@
+package net.mehvahdjukaar.supplementaries.integration.fabric;
+
+public class CreateCompatImpl {
+
+    //TODO
+    public static void setup() {
+    }
+
+    public static void setupClient() {
+    }
+
+    public static void init() {
+    }
+}
