734b62bbead1890d4b31205d227104a5e1e69d2d
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-03-31 22:30:50 +0200

    charge recipe stuff

--- common/src/main/java/net/mehvahdjukaar/supplementaries/common/items/crafting/AddChargeRecipe.java
+++ common/src/main/java/net/mehvahdjukaar/supplementaries/common/items/crafting/AddChargeRecipe.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.mehvahdjukaar.supplementaries.common.items.RopeArrowItem;
+import net.mehvahdjukaar.supplementaries.common.misc.mob_container.BucketHelper;
 import net.mehvahdjukaar.supplementaries.reg.ModComponents;
 import net.mehvahdjukaar.supplementaries.reg.ModRecipes;
 import net.mehvahdjukaar.supplementaries.reg.ModRegistry;
@@ -25,14 +26,16 @@ public class AddChargeRecipe extends CustomRecipe {
     private final Ingredient targetCharge;
     private final Item result;
     private final int chargesPerItem;
+    private final boolean canOverflow;
 
     public AddChargeRecipe(CraftingBookCategory category, Ingredient arrow, Ingredient rope, Item result,
-                           int chargesPerItem) {
+                           int chargesPerItem, boolean canOverflow) {
         super(category);
         this.targetItem = arrow;
         this.targetCharge = rope;
         this.result = result;
         this.chargesPerItem = chargesPerItem;
+        this.canOverflow = canOverflow;
     }
 
     @Override
@@ -40,7 +43,7 @@ public class AddChargeRecipe extends CustomRecipe {
 
         ItemStack arrow = null;
         ItemStack rope = null;
-        int chargesItCanAdd = 0;
+        int newTotalCharges = 0;
 
         for (int i = 0; i < inv.size(); ++i) {
             ItemStack stack = inv.getItem(i);
@@ -49,31 +52,32 @@ public class AddChargeRecipe extends CustomRecipe {
                     return false;
                 }
                 arrow = stack;
-                chargesItCanAdd += stack.getOrDefault(ModComponents.CHARGES.get(), 0);
+                newTotalCharges += stack.getOrDefault(ModComponents.CHARGES.get(), 0);
             } else if (targetCharge.test(stack)) {
                 rope = stack;
-                chargesItCanAdd += chargesPerItem;
+                newTotalCharges += chargesPerItem;
             } else if (!stack.isEmpty()) return false;
         }
-        return arrow != null && rope != null && chargesItCanAdd <=
-                arrow.getOrDefault(ModComponents.MAX_CHARGES.get(), 0);
+        return arrow != null && rope != null && (canOverflow || newTotalCharges <=
+                arrow.getOrDefault(ModComponents.MAX_CHARGES.get(), 0));
     }
 
     @Override
     public ItemStack assemble(CraftingInput inv, HolderLookup.Provider access) {
-        int chargesItCanAdd = 0;
+        int newTotalCharges = 0;
         ItemStack arrow = null;
         for (int i = 0; i < inv.size(); ++i) {
             ItemStack stack = inv.getItem(i);
             if (targetItem.test(stack)) {
                 arrow = stack;
-                chargesItCanAdd += stack.getOrDefault(ModComponents.CHARGES.get(), 0);
+                newTotalCharges += stack.getOrDefault(ModComponents.CHARGES.get(), 0);
             } else if (targetCharge.test(stack)) {
-                chargesItCanAdd += chargesPerItem;
+                newTotalCharges += chargesPerItem;
             }
         }
         ItemStack returnArrow = arrow.transmuteCopy(result, 1);
-        returnArrow.set(ModComponents.CHARGES.get(), chargesItCanAdd);
+        returnArrow.set(ModComponents.CHARGES.get(), Math.min(newTotalCharges,
+                returnArrow.getOrDefault(ModComponents.MAX_CHARGES.get(), 0)));
         return returnArrow;
 
     }
@@ -95,7 +99,8 @@ public class AddChargeRecipe extends CustomRecipe {
                 Ingredient.CODEC.fieldOf("ingredient").forGetter((recipe) -> recipe.targetItem),
                 Ingredient.CODEC.fieldOf("charge").forGetter((recipe) -> recipe.targetCharge),
                 BuiltInRegistries.ITEM.byNameCodec().fieldOf("result").forGetter((recipe) -> recipe.result),
-                Codec.INT.optionalFieldOf("charges_per_item", 1).forGetter((recipe) -> recipe.chargesPerItem)
+                Codec.INT.optionalFieldOf("charges_per_item", 1).forGetter((recipe) -> recipe.chargesPerItem),
+                Codec.BOOL.optionalFieldOf("can_overfill", false).forGetter((recipe) -> recipe.canOverflow)
         ).apply(instance, AddChargeRecipe::new));
 
         private static final StreamCodec<RegistryFriendlyByteBuf, AddChargeRecipe> STREAM_CODEC = StreamCodec.composite(
@@ -104,6 +109,7 @@ public class AddChargeRecipe extends CustomRecipe {
                 Ingredient.CONTENTS_STREAM_CODEC, recipe -> recipe.targetCharge,
                 ByteBufCodecs.registry(Registries.ITEM), recipe -> recipe.result,
                 ByteBufCodecs.VAR_INT, recipe -> recipe.chargesPerItem,
+                ByteBufCodecs.BOOL, recipe -> recipe.canOverflow,
                 AddChargeRecipe::new);
 
         @Override
