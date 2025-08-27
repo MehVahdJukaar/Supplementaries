c3dab67ddec1f102d2a64e018e482aea8955bc30
MehVahdJukaar <matteogozzi1998@gmail.com>
2025-08-28 01:22:10 +0200

    more tags

--- neoforge/src/main/java/net/mehvahdjukaar/supplementaries/neoforge/ReplaceRopeByConfigModifier.java
+++ neoforge/src/main/java/net/mehvahdjukaar/supplementaries/neoforge/ReplaceRopeByConfigModifier.java
@@ -12,6 +12,7 @@ import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.neoforged.neoforge.common.loot.IGlobalLootModifier;
 import net.neoforged.neoforge.common.loot.LootModifier;
+import net.neoforged.neoforge.event.level.BlockEvent;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
