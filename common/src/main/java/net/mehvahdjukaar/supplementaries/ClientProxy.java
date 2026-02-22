7146f7044ab3fbcbf674d6e36da45c155c4755c2
MehVahdJukaar <matteogozzi1998@gmail.com>
2026-02-22 19:17:47 +0100

    gradle stuff

--- common/src/main/java/net/mehvahdjukaar/supplementaries/ClientProxy.java
+++ common/src/main/java/net/mehvahdjukaar/supplementaries/ClientProxy.java
@@ -1,14 +1,11 @@
 package net.mehvahdjukaar.supplementaries;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.world.entity.ai.goal.WaterAvoidingRandomStrollGoal;
-import net.minecraft.world.entity.ai.util.GoalUtils;
 import net.minecraft.world.entity.player.Player;
 
 public class ClientProxy {
 
    public static Player getLocalPlayer() {
-
         return Minecraft.getInstance().player;
     }
 }
