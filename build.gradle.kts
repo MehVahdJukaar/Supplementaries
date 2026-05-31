d6b570193f224c42881d46f7ae696fd5b26988dd
MehVahdJukaar <matteogozzi1998@gmail.com>
2026-05-31 12:18:03 +0200

    new candle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
-import org.apache.commons.io.output.ByteArrayOutputStream
-import org.gradle.internal.extensions.core.serviceOf
-import java.nio.charset.Charset
 
 plugins {
     id("com.possible-triangle.core")
     id("com.possible-triangle.common") apply false
     id("com.possible-triangle.fabric") apply false
     id("com.possible-triangle.neoforge") apply false
-    id("net.mehvahdjukaar.candlelight") version "1.1.6" apply false
+    id("net.mehvahdjukaar.candlelight") version "1.1.13" apply false
     id("dev.mixinmcp.decompile") version "0.9.0" apply false
 
 }
@@ -104,45 +101,5 @@ subprojects {
         maven { url = uri("https://raw.githubusercontent.com/Fuzss/modresources/main/maven") } // Fuzss' Mod Resources
         maven { url = uri("https://maven.jamieswhiteshirt.com/libs-release") } // Jamie's Mods
         maven { url = uri("https://maven.ryanhcode.dev/releases") }
-    }
-}
-
-
-
-tasks.register("buildAndPublishAll") {
-    group = "build"
-    description = "Runs clean, build, publish for all projects"
-
-    dependsOn(subprojects.map { it.tasks.named("clean") })
-    dependsOn(subprojects.map { it.tasks.named("build") })
-    dependsOn(subprojects.map { it.tasks.named("curseforge") })
-    dependsOn(subprojects.map { it.tasks.named("modrinth") })
-
-    finalizedBy("gitTag")
-}
-val mod_version: String by extra
-
-tasks.register("gitTag") {
-    group = "build"
-    doLast {
-        val execOps = serviceOf<ExecOperations>() // Fetches the service
-        val tag = mod_version
-        val stdout = ByteArrayOutputStream()
-
-        execOps.exec {
-            commandLine("git", "tag", "-l", tag)
-            standardOutput = stdout
-        }
-
-        if (!stdout.toString(Charset.defaultCharset()).trim().isEmpty()) {
-            logger.warn("Git tag '${tag}' already exists")
-        } else {
-            execOps.exec {
-                commandLine("git", "tag", "-a", tag, "-m", "Release $tag")
-            }
-            execOps.exec {
-                commandLine("git", "push", "origin", tag)
-            }
-        }
     }
 }
